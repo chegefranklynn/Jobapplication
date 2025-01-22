dffa3e7cca84d8c05a25363589df7115cf2c3a78
frantez <chegefranklynn@gmail.com>
2025-01-22 15:30:17 +0300

    #

--- src/php/scraperInterface.php
+++ src/php/scraperInterface.php
@@ -8,8 +8,7 @@ interface ScraperInterface
      * Scrape the given URL and return an array of job listings.
      *
      * @param string $url The URL to scrape.
-     * @return array An array of job listings.
+     * @return array<int, array{title: string, company: string, location: string, description: string}> An array of job listings where each listing contains a title, company, location, and description.
      */
     public function scrape(string $url): array;
 }
- 
